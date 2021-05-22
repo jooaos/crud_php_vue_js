8aba853fde9a8139c4d4aed24f086ecff7c38651
João Victor <jovictors.soares@gmail.com>
2021-05-22 00:29:30 -0400

    Adicao dos dados necessarios no model Pessoas

--- crud_laravel/app/Pessoas.php
+++ crud_laravel/app/Pessoas.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pessoas extends Model
 {
-    //
+    protected $table = 'pessoas';
+    protected $fillable = ['nome_usuario', 'email_usuario', 'fk_categoria_id'];
+    protected $primaryKey = 'codigo';
+    public $timestamps = false;
 }
